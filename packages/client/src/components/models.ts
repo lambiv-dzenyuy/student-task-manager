6c14b908711e084486568eac38f67108cca0dd2d
lambiv-dzenyuy <gillslambiv@gmail.com>
2023-03-29 20:52:09 +0100

    feat(client): add interface for task

--- packages/client/src/components/models.ts
+++ packages/client/src/components/models.ts
@@ -1,8 +1,7 @@
-export interface Todo {
-  id: number;
-  content: string;
-}
+export interface Task { id: number; title: string; description: string; status: string; completed : boolean; }
 
 export interface Meta {
   totalCount: number;
 }
+
+export interface Todo { id: number; value: string; list: string; }
