aafe39c546bd46dcb727378011073ca0793f5c05
lambiv-dzenyuy <gillslambiv@gmail.com>
2023-04-04 10:26:25 +0100

    feat(client): update task drop boards

--- packages/client/src/pages/IndexPage.vue
+++ packages/client/src/pages/IndexPage.vue
@@ -15,7 +15,11 @@ class="rounded-borders bg-white   drop-zone"
     <q-card-section>
         <div class="text-h6">To-Do</div>
     </q-card-section>
-    <q-scroll-area class="fit">
+    <q-scroll-area
+      visible
+      :vertical-thumb-style="thumbStyle"
+
+      style="height: 84%">
     <q-card
       v-for="item in getList('To-Do')"
       :key="item.id"
@@ -45,9 +49,11 @@ class="rounded-borders bg-white   drop-zone"
 
 
 
-            <span>Due day: </span>{{ item.createdAt }}
+            <span>Created: </span>{{
+             Math.ceil(((new Date()).getTime()  - ( new Date(item.createdAt)).getTime())/(1000*3600*24))
 
-
+            }}
+            <span>day ago </span>
         </q-card-section>
 
 
@@ -55,28 +61,147 @@ class="rounded-borders bg-white   drop-zone"
 
     </q-scroll-area>
 
+    <q-card-actions class="q-pa-none q-ma-none text-secondary ">
+        <q-btn flat dense class="text-capitalize"  :icon="mdiPlus">
+          New Item
 
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+
+    <q-card
+
+flat bordered
+class="rounded-borders bg-white   drop-zone"
+    @drop="onDrop($event, 'In Progress')"
+    @dragenter.prevent @dragover.prevent >
+    <q-card-section>
+        <div class="text-h6">In Progress</div>
+    </q-card-section>
+    <q-scroll-area
+      visible
+      :vertical-thumb-style="thumbStyle"
+
+      style="height: 84%">
+    <q-card
+      v-for="item in getList('In Progress')"
+      :key="item.id"
+        bordered
+        flat
+        draggable="true"
+        class="task-card q-ma-sm "
+        @dragstart="startDrag($event, item)" >
+
+
+
+        <q-card-section>
+
+            <q-btn
+            flat
+            dense
+            rounded
+            disable
+            size="sm"
+            :color="item.priority==='high' ? 'white' : 'black' "
+            :class="`text-capitalize bg-${ item.priority==='high' ? 'negative' : item.priority==='medium' ? 'accent' : 'positive'} q-px-md text-weight-medium`">
+            {{ item.priority }}
+          </q-btn>
+
+
+            <span class="text-weight-medium">{{ item.description }}</span>
+
+
+
+            <span>Created: </span>{{
+             Math.ceil(((new Date()).getTime()  - ( new Date(item.createdAt)).getTime())/(1000*3600*24))
+
+            }}
+            <span>day ago </span>
+        </q-card-section>
+
+
+      </q-card>
+
+    </q-scroll-area>
+
+    <q-card-actions class="q-pa-none q-ma-none text-secondary ">
+        <q-btn flat dense class="text-capitalize"  :icon="mdiPlus">
+          New Item
+
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+
+    <q-card
+
+flat bordered
+class="rounded-borders bg-white   drop-zone"
+    @drop="onDrop($event, 'Done')"
+    @dragenter.prevent @dragover.prevent >
+    <q-card-section>
+        <div class="text-h6">Done</div>
+    </q-card-section>
+    <q-scroll-area
+      visible
+      :vertical-thumb-style="thumbStyle"
+
+      style="height: 84%">
+    <q-card
+      v-for="item in getList('Done')"
+      :key="item.id"
+        bordered
+        flat
+        draggable="true"
+        class="task-card q-ma-sm "
+        @dragstart="startDrag($event, item)" >
+
+
+
+        <q-card-section>
+
+            <q-btn
+            flat
+            dense
+            rounded
+            disable
+            size="sm"
+            :color="item.priority==='high' ? 'white' : 'black' "
+            :class="`text-capitalize bg-${ item.priority==='high' ? 'negative' : item.priority==='medium' ? 'accent' : 'positive'} q-px-md text-weight-medium`">
+            {{ item.priority }}
+          </q-btn>
+
+
+            <span class="text-weight-medium">{{ item.description }}</span>
+
+
+
+            <span>Created: </span>{{
+             Math.ceil(((new Date()).getTime()  - ( new Date(item.createdAt)).getTime())/(1000*3600*24))
+
+            }}
+            <span>day ago </span>
+        </q-card-section>
+
+
+      </q-card>
+
+    </q-scroll-area>
+
+    <q-card-actions class="q-pa-none q-ma-none text-secondary ">
+        <q-btn flat dense class="text-capitalize"  :icon="mdiPlus">
+          New Item
+
+        </q-btn>
+      </q-card-actions>
     </q-card>
 
 
 
-  <div class="drop-zone " @drop="onDrop($event, 'In Progress')" @dragenter.prevent @dragover.prevent>
-    <div
-v-for="item in getList('In Progress')" :key="item.id" draggable="true" class="drag-el bg-accent q-ma-xl"
-      @dragstart="startDrag($event, item)">
-      {{ item.title }}
-    </div>
-  </div>
-  <div class="drop-zone " @drop="onDrop($event, 'Done')" @dragenter.prevent @dragover.prevent>
-    <div
-v-for="item in getList('Done')" :key="item.id" draggable="true" class="drag-el bg-accent q-ma-xl"
-      @dragstart="startDrag($event, item)">
-      {{ item.title }}
-    </div>
-  </div>
-  </div>
 
 
+</div>
 </div>
 </q-page>
 </template>
@@ -84,6 +209,7 @@ v-for="item in getList('Done')" :key="item.id" draggable="true" class="drag-el b
 
 
 <script lang="ts" setup >
+import { mdiPlus } from '@quasar/extras/mdi-v6';
 import { api } from 'src/boot/axios';
 import { onBeforeMount, onErrorCaptured, ref } from 'vue';
 import { Task } from '../components/models'
@@ -144,15 +270,27 @@ console.log('on drop', 'id', itemID);
 
   }
 }
+const thumbStyle = {
+        right: '4px',
+        borderRadius: '7px',
+        backgroundColor: '#000235',
+        width: '4px',
+        opacity: 0.75
+      };
 
+    const   barStyle ={
+        right: '2px',
+        borderRadius: '9px',
+        backgroundColor: '#027be3',
+        width: '8px',
+        opacity: 0.2
+      }
 </script>
 
 <style lang="scss" scoped>
 .drop-zone{
-  height: 80vh;
+  height: 94vh;
   width: 350px;
 }
-.q-scroll-area{
-  height: 60vh;
-}
+
 </style>
