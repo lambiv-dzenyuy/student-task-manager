8804c9015d5e651b63aa8f6abadc3128ebb233f1
gills-lambv <gillslambiv@gmail.com>
2023-04-10 22:07:55 +0100

    feat(client): add usability to create user

--- packages/client/src/pages/signup.vue
+++ packages/client/src/pages/signup.vue
@@ -1,46 +1,36 @@
 <template>
   <q-page
-      class="window-height bg-primary window-width row justify-center items-center"
-    >
-
-      <div class="row q-pa-lg">
-
-        <div class="col  ">
-          <q-card class="shadow-24 form rounded-borders" >
+      class="window-height bg-primary window-width row justify-center items-center" >
+      <div class="row ">
+        <div class=" wrap justify-start items-start content-start  ">
+          <q-card class="shadow-24 q-pl-none form rounded-borders" >
             <q-card-section header class="text-black center text-center text-h5 text-grey-8">Create an account</q-card-section>
-
             <q-card-section>
-
-
               <q-form class="q-px-sm ">
                 <q-input
-
-v-model="email"
-square
-outlined
-clearable
-type="text"
-class="text-red q-pa-md"
-label="First Name"
-
->
-</q-input>
-<q-input
-
-v-model="email"
-square
-outlined
-clearable
-type="text"
-class="text-red q-pa-md"
-label="Last Name"
-
->
-
-</q-input>
+                  v-model="data.firstName"
+                  dense
+                  square
+                  outlined
+                  clearable
+                  type="text"
+                  class="text-red q-pa-md"
+                  label="First Name">
+                </q-input>
                 <q-input
-
-                         v-model="email"
+                  v-model="data.lastName"
+                  dense
+                  square
+                  outlined
+                  clearable
+                  type="text"
+                  class="text-red q-pa-md"
+                  label="Last Name"
+                  >
+                </q-input>
+                <q-input
+                  v-model="data.email"
+                         dense
                          square
                          outlined
                          clearable
@@ -56,25 +46,23 @@ label="Last Name"
 
 
                 <q-input
-
-                         v-model="password"
-                         square
-                         clearable
-                         lazy-rules
-                         bordered
-                         outlined
-                         class="text-red q-pa-md"
-                         type="password"
-                         label="Password">
-
-                  <template #prepend>
-                    <q-icon name="lock" />
-                  </template>
-
+                  v-model="password"
+                  dense
+                  square
+                  clearable
+                  lazy-rules
+                  bordered
+                  outlined
+                  class="text-red q-pa-md"
+                  type="password"
+                  label="Password">
+                    <template #prepend>
+                      <q-icon name="lock" />
+                    </template>
                 </q-input>
                 <q-input
-
-                         v-model="password"
+                v-model="repeatedPassword"
+                         dense
                          square
                          clearable
                          lazy-rules
@@ -103,21 +91,20 @@ label="Last Name"
                     @click="submit"                      />
             </q-card-actions>
             <q-card-section
-
                 class="text-center q-pa-sm">
-              <p  class="text-primary">Already have an account?</p>
+              <p class="text-primary cursor-pointer" @click="navigateToLogin()">Already have an account?</p>
             </q-card-section>
           </q-card>
         </div>
 
-        <div class="col q-ma-xl q-pa-xl  landing-image text-message-section">
+        <div class="col q-pa-xl q-pa-xl  landing-image text-message-section">
 
-    <div class="q-px-auto q-mb-xl text-center text-white text-h3">
+    <div class="q-px-auto q-pb-xl text-center text-white text-h3">
 
   <p>Start Tracking</p>
   <p>Your Tasks</p>
     </div>
-    <q-img src="../../public/icons/landing.avif" class="q-ml-xl q-pa-xl float-left"/>
+    <q-img src="/public/icons/landing.avif" class="q-ml-xl q-pa-xl float-left"/>
   </div>
 
       </div>
@@ -131,24 +118,50 @@ label="Last Name"
   import { api } from 'src/boot/axios';
   import { ref } from 'vue';
   import { useAuthStore } from 'src/stores/auth';
+import { Student } from 'src/components/models';
+
+// inside of a Vue file
+import { useQuasar } from 'quasar'
+
+
+  const $q = useQuasar()
+
+  function triggerNotification (type : string, message : string) {
+        $q.notify({
+          type: type,
+          message: message,
+          // position: 'top-right',
+        })
+
+
+      }
 
   const auth = useAuthStore()
 
   const password = ref('')
-  const email = ref('')
+  const repeatedPassword = ref('')
+  const data = ref<Student>({} as Student)
 
   const router = useRouter();
 
+  function navigateToLogin (){
+    router.push({name :'login'})
+  }
+
   function submit(){
-    api.post('auth/login',
-    {email : email.value, password : password.value})
+    api.post('user',
+    {...data.value, password : password.value, avatar : "dfa"})
     .then(response  => {
       console.log(response);
 
-        if(response.status >=200 && response.status <300 && response.data.access_token && response.data.userData){
-          auth.set(response.data.access_token, response.data.userData)
+        if(response.status >=200 && response.status <300){
+         triggerNotification('positive', 'Account created successfully')
         }
-        router.push({name: 'dashboard'})
+        else
+        {
+          triggerNotification('negative', String(response.status) as string)
+        }
+        router.push({name: 'login'})
 
     })
   }
