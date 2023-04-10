28d78512174d23df3f0682585fa1b43c0b826bf4
gills-lambv <gillslambiv@gmail.com>
2023-04-10 23:26:18 +0100

    feat(client): create task

--- packages/client/src/components/create-task.vue
+++ packages/client/src/components/create-task.vue
@@ -1,41 +1,41 @@
 <template>
-
   <q-card class="q-dialog-plugin rounded-borders ">
     <q-card-section class="text-center">
       Create Task
     </q-card-section>
     <q-card-section>
-
-
-<q-form class="q-px-sm ">
-<q-input
-
-  v-model="projectDetails.title"
-  square
-  outlined
-  clearable
-type="text"
-class="text-red q-pa-md"
-label="Title"
-
->
-</q-input>
-<q-input
-
-v-model="projectDetails .description"
-square
-outlined
-clearable
-type="textarea"
-class="text-red q-pa-md"
-label="Description"
-
->
-
-</q-input>
-<q-select
-
-         v-model="projectDetails .title"
+      <q-form class="q-px-sm ">
+        <q-select
+         v-model="taskDetails.projectId"
+         square
+         clearable
+         lazy-rules
+         bordered
+         outlined
+         class="text-red q-pa-md"
+         :options="projects.map(project => project.id)"
+         label="project">
+      </q-select>
+      <q-input
+        v-model="taskDetails.title"
+        square
+        outlined
+        clearable
+        type="text"
+        class="text-red q-pa-md"
+        label="Title">
+      </q-input>
+      <q-input
+        v-model="taskDetails .description"
+        square
+        outlined
+        clearable
+        type="textarea"
+        class="text-red q-pa-md"
+        label="Description"  >
+      </q-input>
+      <q-select
+         v-model="taskDetails .priority"
          square
          clearable
          lazy-rules
@@ -44,13 +44,9 @@ label="Description"
          :options="['High','Medium', 'Low']"
          class="text-red q-pa-md"
          label="Priority">
-
-
-
-</q-select>
-<q-select
-
-         v-model="projectDetails .studentId"
+      </q-select>
+      <q-select
+         v-model="taskDetails.status"
          square
          clearable
          lazy-rules
@@ -59,58 +55,81 @@ label="Description"
          class="text-red q-pa-md"
          :options="['To-Do','In Progress', 'Done']"
          label="status">
-
-
-</q-select>
-<q-input
-
-         v-model="projectDetails.title"
+      </q-select>
+      <q-input
+         v-model="taskDetails.startDate"
          square
          outlined
          clearable
          type="date"
          class="text-red q-pa-md"
          label="Start Date"
-
          >
-
-</q-input>
-<q-input
-
-         v-model="projectDetails.title"
+      </q-input>
+      <q-input
+         v-model="taskDetails.endDate"
          square
          outlined
          clearable
          type="date"
          class="text-red q-pa-md"
          label="End Date"
-
          >
-
-</q-input>
-
-
-
-</q-form>
-</q-card-section>
-
-    <q-card-actions align="center">
-      <q-btn color="secondary" label="Save Task"  />
+      </q-input>
+    </q-form>
+  </q-card-section>
+  <q-card-actions align="center">
+      <q-btn color="secondary" label="Save Task" @click="submitTask()" />
       <q-btn color="secondary" label="Cancel" @click="$emit('open-create-task-dialog', true)" />
     </q-card-actions>
   </q-card>
 </template>
 
 <script lang='ts' setup>
-import { Project } from './models';
+import { useAuthStore } from 'src/stores/auth';
 import { ref } from 'vue';
+import { api } from 'src/boot/axios';
+import { appNotify} from './notify'
+import { Project } from './models';
 
+
+const auth = useAuthStore()
+interface Task {
+  title: string,
+  description: string,
+  status: string,
+  priority: string,
+  endDate: Date,
+  startDate: Date,
+  projectId: string
+}
+const taskDetails = ref<Task>({} as Task)
+
+const projects = ref<Project[]>([])
 
 defineEmits<{
 (e: 'open-create-task-dialog', va: boolean): boolean
-(e: 'update', value: string): void
 }>()
 
-const projectDetails = ref<Project>({} as Project)
+api.get(`projects/${auth.authUser?.id}`,  { headers: {
+           Authorization:'Bearer ' + auth.token,
+          'x-access-token': auth.token
+        }}).then(res => {
+  projects.value = res.data;
+projects.value.map(item => console.log((new Date(item.createdAt)).toDateString()))
 
+});
+
+function submitTask(){
+  api.post('tasks', {...taskDetails.value, studentId : auth.authUser?.id }, { headers: {
+           Authorization:'Bearer ' + auth.token,
+          'x-access-token': auth.token
+        }}).then(response => {
+          if(response.status >=200 && response.status <300){
+  appNotify.success
+          }
+          else appNotify.error
+
+});
+}
 </script>
