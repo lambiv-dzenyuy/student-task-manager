a28e06727f9a7b74964e8402069b4cff95048336
gills-lambv <gillslambiv@gmail.com>
2023-04-10 23:23:42 +0100

    feat(client): add notify helper

---
+++ packages/client/src/components/notify.ts
@@ -0,0 +1,18 @@
+import { Notify } from 'quasar';
+
+export const appNotify = {
+  success: (message?: string) => {
+    Notify.create({
+      message: message ?? 'Operation completed successfully',
+      type: 'positive',
+      position: 'center'
+    });
+  },
+  error: (message?: string) => {
+    Notify.create({
+      message: message ?? 'An error ocurred',
+      type: 'negative',
+      position: 'top-right'
+    });
+  },
+};
