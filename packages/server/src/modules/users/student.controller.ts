46a040a32ed3ed8a1b239383b24a9fbc0246ab08
gills-lambv <gillslambiv@gmail.com>
2023-04-22 11:50:27 +0100

    feat(server): add update user end point

--- packages/server/src/modules/users/student.controller.ts
+++ packages/server/src/modules/users/student.controller.ts
@@ -1,34 +1,39 @@
-import { Body, Controller, Get, HttpCode, Logger, Param, Patch, Post, UnauthorizedException } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { retry } from "rxjs";
-import { StudentService} from "./student.service";
-import { Public } from "../auth/skip.auth";
-import { CreateStudentDto } from "./dto/create-student.dto";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Logger,
+  Param,
+  Patch,
+  Post,
+  UnauthorizedException
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { retry } from 'rxjs';
+import { StudentService } from './student.service';
+import { Public } from '../auth/skip.auth';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
 
-@Controller("api/v1/user")
+@Controller('api/v1/user')
 export class StudentController {
-  constructor(private readonly studentService: StudentService, private jwtService: JwtService) {}
-
-  @Get()
-  async findAll() {
-    return this.studentService.findAll();
-  }
-
- 
-
-  @Get()
-  students() {
-    Logger.log('students');
-    return this.studentService.findAll();
-  }
+  constructor(
+    private readonly studentService: StudentService,
+    private jwtService: JwtService
+  ) {}
 
   @Public()
   @Post()
   @HttpCode(204)
-  create(@Body() studentDto : CreateStudentDto) {
-   return this.studentService.create(studentDto);
+  create(@Body() studentDto: CreateStudentDto) {
+    return this.studentService.create(studentDto);
   }
-
+  @Patch(':id')
+  @HttpCode(204)
+  update(@Body() studentDto: UpdateStudentDto, @Param('id') studentId: string) {
+    return this.studentService.update(+studentId, studentDto);
+  }
   @Get(':id')
   findOne(@Param() params): string {
     console.log(params.id);
--- packages/server/src/modules/users/student.service.ts
+++ packages/server/src/modules/users/student.service.ts
@@ -1,39 +1,36 @@
-import { HttpException, HttpStatus, Injectable, Logger } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { PrismaService } from "src/prisma/prisma.service";
-import { CreateStudentDto } from "./dto/create-student.dto";
-import { AuthService } from "../auth/auth.service";
-
-
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { AuthService } from '../auth/auth.service';
+import { UpdateStudentDto } from './dto/update-student.dto';
 
 @Injectable()
 export class StudentService {
+  constructor(private readonly prisma: PrismaService) {}
 
-  constructor( private readonly prisma : PrismaService){}
-    
   findOne(email: string) {
-   return this.prisma.student.findUnique({where : {email}});
- }
+    return this.prisma.student.findUnique({ where: { email } });
+  }
 
- findOne1(id: string) {
-   return this.prisma.task.findUnique({ where: { id: id } });
- }
- create(createStudentDto : CreateStudentDto ){
-  
- 
-try{
-  this.prisma.student.create({
-    data : createStudentDto
-  })
-}
-catch(err){
-  throw new HttpException(`Student with id: ${createStudentDto.email} already exist.`, HttpStatus.FORBIDDEN);
-} 
-   
-  
-}
- findAll() {
-   return this.prisma.student.findMany();
- }
-}
+  findOne1(id: string) {
+    return this.prisma.task.findUnique({ where: { id } });
+  }
+  create(createStudentDto: CreateStudentDto) {
+    try {
+      this.prisma.student.create({
+        data: createStudentDto
+      });
+    } catch (err) {
+      throw new HttpException(
+        `Student with id: ${createStudentDto.email} already exist.`,
+        HttpStatus.FORBIDDEN
+      );
+    }
+  }
 
+  update(id : number, updateStudentDto : UpdateStudentDto){
+    return this.prisma.student.update({ where : { id }, data : updateStudentDto})
+  }
+  
+}
