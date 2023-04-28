15cb0bd38b838e191e254adb7ec1d08310800e26
gills-lambv <gillslambiv@gmail.com>
2023-04-28 19:38:31 +0100

    fix(client): fix formatting

--- packages/client/src/components/create-task.vue
+++ packages/client/src/components/create-task.vue
@@ -1,112 +1,99 @@
 <template>
-   <q-dialog ref="dialogRef" @hide="onDialogHide">
-
-  <q-card class="q-dialog-plugin rounded-borders">
-    <q-card-section class="text-center"> Create Task </q-card-section>
-    <q-card-section>
-      <q-form class="q-px-sm q-pa-md">
-        <q-select
-          v-model="projectTitle"
-          clearable
-          dense
-          outlined
-          :options="projectNames"
-          label="project"
-          :rules="[(value) => !!value || $t('firstNameIsRequired')]"
-        >
-        </q-select>
-        <q-input
-          v-model="taskDetails.title"
-          square
-          dense
-          outlined
-          clearable
-          label="Title"
-          :rules="[(value) => !!value || $t('firstNameIsRequired')]"
-        >
-        </q-input>
-        <q-input
-          v-model="taskDetails.description"
-          square
-          dense
-          outlined
-          clearable
-          type="textarea"
-          label="Description"
-          class="q-py-md"
-        >
-        </q-input>
-        <q-select
-          v-model="taskDetails.priority"
-          square
-          dense
-          clearable
-          lazy-rules
-          bordered
-          outlined
-          :options="['High', 'Medium', 'Low']"
-          label="Priority"
-          :rules="[(value) => !!value || $t('firstNameIsRequired')]"
-        >
-        </q-select>
-        <q-select
-          v-model="taskDetails.status"
-          square
-          clearable
-          dense
-          lazy-rules
-          bordered
-          outlined
-          :options="['To-Do', 'In Progress', 'Done']"
-          label="status"
-
-        >
-        </q-select>
-        <q-input
-          v-model="taskDetails.startDate"
-          square
-          outlined
-          dense
-          clearable
-          class="q-py-md"
-          type="date"
-          label="Start Date"
-        >
-        </q-input>
-        <q-input
-          v-model="taskDetails.endDate"
-          square
-          dense
-          outlined
-          clearable
-          type="date"
-          class="q-py-md"
-          label="End Date"
-        >
-        </q-input>
-      </q-form>
-    </q-card-section>
-    <q-card-actions align="center">
-      <q-btn
-        color="secondary"
-        label="Save Task"
-        @click="
-          submitTask();
-
-        "
-      />
-      <q-btn
-        color="secondary"
-        label="Cancel"
-        @click="onDialogCancel"
-      />
-    </q-card-actions>
-  </q-card>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin rounded-borders">
+      <q-card-section class="text-center"> Create Task </q-card-section>
+      <q-card-section>
+        <q-form class="q-px-sm q-pa-md">
+          <q-select
+            v-model="projectTitle"
+            clearable
+            dense
+            outlined
+            :options="projectNames"
+            label="project"
+            :rules="[(value) => !!value || $t('firstNameIsRequired')]"
+          >
+          </q-select>
+          <q-input
+            v-model="taskDetails.title"
+            square
+            dense
+            outlined
+            clearable
+            label="Title"
+            :rules="[(value) => !!value || $t('firstNameIsRequired')]"
+          >
+          </q-input>
+          <q-input
+            v-model="taskDetails.description"
+            square
+            dense
+            outlined
+            clearable
+            type="textarea"
+            label="Description"
+            class="q-py-md"
+          >
+          </q-input>
+          <q-select
+            v-model="taskDetails.priority"
+            square
+            dense
+            clearable
+            lazy-rules
+            bordered
+            outlined
+            :options="['High', 'Medium', 'Low']"
+            label="Priority"
+            :rules="[(value) => !!value || $t('firstNameIsRequired')]"
+          >
+          </q-select>
+          <q-select
+            v-model="taskDetails.status"
+            square
+            clearable
+            dense
+            lazy-rules
+            bordered
+            outlined
+            :options="['To-Do', 'In Progress', 'Done']"
+            label="status"
+          >
+          </q-select>
+          <q-input
+            v-model="taskDetails.startDate"
+            square
+            outlined
+            dense
+            clearable
+            class="q-py-md"
+            type="date"
+            label="Start Date"
+          >
+          </q-input>
+          <q-input
+            v-model="taskDetails.endDate"
+            square
+            dense
+            outlined
+            clearable
+            type="date"
+            class="q-py-md"
+            label="End Date"
+          >
+          </q-input>
+        </q-form>
+      </q-card-section>
+      <q-card-actions align="center">
+        <q-btn color="secondary" label="Save Task" @click="submitTask()" />
+        <q-btn color="secondary" label="Cancel" @click="onDialogCancel" />
+      </q-card-actions>
+    </q-card>
   </q-dialog>
 </template>
 
 <script lang="ts" setup>
-import { useDialogPluginComponent } from 'quasar'
+import { useDialogPluginComponent } from 'quasar';
 import { useAuthStore } from 'src/stores/auth';
 import { computed, ref } from 'vue';
 import { api } from 'src/boot/axios';
@@ -123,14 +110,14 @@ interface Task {
   startDate: Date;
   projectId: string;
 }
-const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginComponent()
+const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
+  useDialogPluginComponent();
 
 const auth = useAuthStore();
 const router = useRouter();
 
 const projectTitle = ref('');
 const taskDetails = ref<Task>({} as Task);
-
 
 const projects = ref<Project[]>([]);
 const projectNames = computed(() => {
